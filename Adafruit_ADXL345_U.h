df51e8109dd9e58d7a44d6161c6aed4739a2244d
siddacious <nospam187+github@gmail.com>
2020-04-27 11:46:31 -0700

    actionified, formatted and doxy'd

--- Adafruit_ADXL345_U.h
+++ Adafruit_ADXL345_U.h
@@ -1,19 +1,26 @@
 /**************************************************************************/
 /*!
-    @file     Adafruit_ADS1015.h
+    @file     Adafruit_ADXL345_U.h
     @author   K. Townsend (Adafruit Industries)
-    @license  BSD (see license.txt)
 
-    This is a library for the Adafruit ADS1015 breakout board
+
+    The ADXL345 is a digital accelerometer with 13-bit resolution, capable
+    of measuring up to +/-16g.  This driver communicate using I2C.
+
+    This is a library for the Adafruit ADXL345 breakout
     ----> https://www.adafruit.com/products/1231
 
     Adafruit invests time and resources providing this open source code,
     please support Adafruit and open-source hardware by purchasing
     products from Adafruit!
 
+    @section license License
+    BSD (see license.txt)
+
     @section  HISTORY
 
-    v1.0  - First release
+    v1.1 - Added Adafruit_Sensor library support
+    v1.0 - First release
 */
 /**************************************************************************/
 
@@ -21,9 +28,9 @@
 #define Adafruit_ADXL345_h
 
 #if ARDUINO >= 100
- #include "Arduino.h"
+#include "Arduino.h"
 #else
- #include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 #include <Adafruit_Sensor.h>
@@ -32,109 +39,120 @@
 /*=========================================================================
     I2C ADDRESS/BITS
     -----------------------------------------------------------------------*/
-    #define ADXL345_DEFAULT_ADDRESS     (0x53)  // Assumes ALT address pin low
+#define ADXL345_DEFAULT_ADDRESS (0x53) ///< Assumes ALT address pin low
 /*=========================================================================*/
 
 /*=========================================================================
     REGISTERS
     -----------------------------------------------------------------------*/
-    #define ADXL345_REG_DEVID               (0x00)    // Device ID
-    #define ADXL345_REG_THRESH_TAP          (0x1D)    // Tap threshold
-    #define ADXL345_REG_OFSX                (0x1E)    // X-axis offset
-    #define ADXL345_REG_OFSY                (0x1F)    // Y-axis offset
-    #define ADXL345_REG_OFSZ                (0x20)    // Z-axis offset
-    #define ADXL345_REG_DUR                 (0x21)    // Tap duration
-    #define ADXL345_REG_LATENT              (0x22)    // Tap latency
-    #define ADXL345_REG_WINDOW              (0x23)    // Tap window
-    #define ADXL345_REG_THRESH_ACT          (0x24)    // Activity threshold
-    #define ADXL345_REG_THRESH_INACT        (0x25)    // Inactivity threshold
-    #define ADXL345_REG_TIME_INACT          (0x26)    // Inactivity time
-    #define ADXL345_REG_ACT_INACT_CTL       (0x27)    // Axis enable control for activity and inactivity detection
-    #define ADXL345_REG_THRESH_FF           (0x28)    // Free-fall threshold
-    #define ADXL345_REG_TIME_FF             (0x29)    // Free-fall time
-    #define ADXL345_REG_TAP_AXES            (0x2A)    // Axis control for single/double tap
-    #define ADXL345_REG_ACT_TAP_STATUS      (0x2B)    // Source for single/double tap
-    #define ADXL345_REG_BW_RATE             (0x2C)    // Data rate and power mode control
-    #define ADXL345_REG_POWER_CTL           (0x2D)    // Power-saving features control
-    #define ADXL345_REG_INT_ENABLE          (0x2E)    // Interrupt enable control
-    #define ADXL345_REG_INT_MAP             (0x2F)    // Interrupt mapping control
-    #define ADXL345_REG_INT_SOURCE          (0x30)    // Source of interrupts
-    #define ADXL345_REG_DATA_FORMAT         (0x31)    // Data format control
-    #define ADXL345_REG_DATAX0              (0x32)    // X-axis data 0
-    #define ADXL345_REG_DATAX1              (0x33)    // X-axis data 1
-    #define ADXL345_REG_DATAY0              (0x34)    // Y-axis data 0
-    #define ADXL345_REG_DATAY1              (0x35)    // Y-axis data 1
-    #define ADXL345_REG_DATAZ0              (0x36)    // Z-axis data 0
-    #define ADXL345_REG_DATAZ1              (0x37)    // Z-axis data 1
-    #define ADXL345_REG_FIFO_CTL            (0x38)    // FIFO control
-    #define ADXL345_REG_FIFO_STATUS         (0x39)    // FIFO status
+#define ADXL345_REG_DEVID (0x00)        ///< Device ID
+#define ADXL345_REG_THRESH_TAP (0x1D)   ///< Tap threshold
+#define ADXL345_REG_OFSX (0x1E)         ///< X-axis offset
+#define ADXL345_REG_OFSY (0x1F)         ///< Y-axis offset
+#define ADXL345_REG_OFSZ (0x20)         ///< Z-axis offset
+#define ADXL345_REG_DUR (0x21)          ///< Tap duration
+#define ADXL345_REG_LATENT (0x22)       ///< Tap latency
+#define ADXL345_REG_WINDOW (0x23)       ///< Tap window
+#define ADXL345_REG_THRESH_ACT (0x24)   ///< Activity threshold
+#define ADXL345_REG_THRESH_INACT (0x25) ///< Inactivity threshold
+#define ADXL345_REG_TIME_INACT (0x26)   ///< Inactivity time
+#define ADXL345_REG_ACT_INACT_CTL                                              \
+  (0x27) ///< Axis enable control for activity and inactivity detection
+#define ADXL345_REG_THRESH_FF (0x28) ///< Free-fall threshold
+#define ADXL345_REG_TIME_FF (0x29)   ///< Free-fall time
+#define ADXL345_REG_TAP_AXES (0x2A)  ///< Axis control for single/double tap
+#define ADXL345_REG_ACT_TAP_STATUS (0x2B) ///< Source for single/double tap
+#define ADXL345_REG_BW_RATE (0x2C)        ///< Data rate and power mode control
+#define ADXL345_REG_POWER_CTL (0x2D)      ///< Power-saving features control
+#define ADXL345_REG_INT_ENABLE (0x2E)     ///< Interrupt enable control
+#define ADXL345_REG_INT_MAP (0x2F)        ///< Interrupt mapping control
+#define ADXL345_REG_INT_SOURCE (0x30)     ///< Source of interrupts
+#define ADXL345_REG_DATA_FORMAT (0x31)    ///< Data format control
+#define ADXL345_REG_DATAX0 (0x32)         ///< X-axis data 0
+#define ADXL345_REG_DATAX1 (0x33)         ///< X-axis data 1
+#define ADXL345_REG_DATAY0 (0x34)         ///< Y-axis data 0
+#define ADXL345_REG_DATAY1 (0x35)         ///< Y-axis data 1
+#define ADXL345_REG_DATAZ0 (0x36)         ///< Z-axis data 0
+#define ADXL345_REG_DATAZ1 (0x37)         ///< Z-axis data 1
+#define ADXL345_REG_FIFO_CTL (0x38)       ///< FIFO control
+#define ADXL345_REG_FIFO_STATUS (0x39)    ///< FIFO status
 /*=========================================================================*/
 
 /*=========================================================================
     REGISTERS
     -----------------------------------------------------------------------*/
-    #define ADXL345_MG2G_MULTIPLIER (0.004)  // 4mg per lsb
+#define ADXL345_MG2G_MULTIPLIER (0.004) ///< 4mg per lsb
 /*=========================================================================*/
 
-/* Used with register 0x2C (ADXL345_REG_BW_RATE) to set bandwidth */
-typedef enum
-{
-  ADXL345_DATARATE_3200_HZ    = 0b1111, // 1600Hz Bandwidth   140µA IDD
-  ADXL345_DATARATE_1600_HZ    = 0b1110, //  800Hz Bandwidth    90µA IDD
-  ADXL345_DATARATE_800_HZ     = 0b1101, //  400Hz Bandwidth   140µA IDD
-  ADXL345_DATARATE_400_HZ     = 0b1100, //  200Hz Bandwidth   140µA IDD
-  ADXL345_DATARATE_200_HZ     = 0b1011, //  100Hz Bandwidth   140µA IDD
-  ADXL345_DATARATE_100_HZ     = 0b1010, //   50Hz Bandwidth   140µA IDD
-  ADXL345_DATARATE_50_HZ      = 0b1001, //   25Hz Bandwidth    90µA IDD
-  ADXL345_DATARATE_25_HZ      = 0b1000, // 12.5Hz Bandwidth    60µA IDD
-  ADXL345_DATARATE_12_5_HZ    = 0b0111, // 6.25Hz Bandwidth    50µA IDD
-  ADXL345_DATARATE_6_25HZ     = 0b0110, // 3.13Hz Bandwidth    45µA IDD
-  ADXL345_DATARATE_3_13_HZ    = 0b0101, // 1.56Hz Bandwidth    40µA IDD
-  ADXL345_DATARATE_1_56_HZ    = 0b0100, // 0.78Hz Bandwidth    34µA IDD
-  ADXL345_DATARATE_0_78_HZ    = 0b0011, // 0.39Hz Bandwidth    23µA IDD
-  ADXL345_DATARATE_0_39_HZ    = 0b0010, // 0.20Hz Bandwidth    23µA IDD
-  ADXL345_DATARATE_0_20_HZ    = 0b0001, // 0.10Hz Bandwidth    23µA IDD
-  ADXL345_DATARATE_0_10_HZ    = 0b0000  // 0.05Hz Bandwidth    23µA IDD (default value)
+/**
+ * @brief Used with register 0x2C (ADXL345_REG_BW_RATE) to set bandwidth
+
+*/
+typedef enum {
+  ADXL345_DATARATE_3200_HZ = 0b1111, ///< 1600Hz Bandwidth   140ï¿½A IDD
+  ADXL345_DATARATE_1600_HZ = 0b1110, ///<  800Hz Bandwidth    90ï¿½A IDD
+  ADXL345_DATARATE_800_HZ = 0b1101,  ///<  400Hz Bandwidth   140ï¿½A IDD
+  ADXL345_DATARATE_400_HZ = 0b1100,  ///<  200Hz Bandwidth   140ï¿½A IDD
+  ADXL345_DATARATE_200_HZ = 0b1011,  ///<  100Hz Bandwidth   140ï¿½A IDD
+  ADXL345_DATARATE_100_HZ = 0b1010,  ///<   50Hz Bandwidth   140ï¿½A IDD
+  ADXL345_DATARATE_50_HZ = 0b1001,   ///<   25Hz Bandwidth    90ï¿½A IDD
+  ADXL345_DATARATE_25_HZ = 0b1000,   ///< 12.5Hz Bandwidth    60ï¿½A IDD
+  ADXL345_DATARATE_12_5_HZ = 0b0111, ///< 6.25Hz Bandwidth    50ï¿½A IDD
+  ADXL345_DATARATE_6_25HZ = 0b0110,  ///< 3.13Hz Bandwidth    45ï¿½A IDD
+  ADXL345_DATARATE_3_13_HZ = 0b0101, ///< 1.56Hz Bandwidth    40ï¿½A IDD
+  ADXL345_DATARATE_1_56_HZ = 0b0100, ///< 0.78Hz Bandwidth    34ï¿½A IDD
+  ADXL345_DATARATE_0_78_HZ = 0b0011, ///< 0.39Hz Bandwidth    23ï¿½A IDD
+  ADXL345_DATARATE_0_39_HZ = 0b0010, ///< 0.20Hz Bandwidth    23ï¿½A IDD
+  ADXL345_DATARATE_0_20_HZ = 0b0001, ///< 0.10Hz Bandwidth    23ï¿½A IDD
+  ADXL345_DATARATE_0_10_HZ =
+      0b0000 ///< 0.05Hz Bandwidth    23ï¿½A IDD (default value)
 } dataRate_t;
 
-/* Used with register 0x31 (ADXL345_REG_DATA_FORMAT) to set g range */
-typedef enum
-{
-  ADXL345_RANGE_16_G          = 0b11,   // +/- 16g
-  ADXL345_RANGE_8_G           = 0b10,   // +/- 8g
-  ADXL345_RANGE_4_G           = 0b01,   // +/- 4g
-  ADXL345_RANGE_2_G           = 0b00    // +/- 2g (default value)
+/**
+ * @brief  Used with register 0x31 (ADXL345_REG_DATA_FORMAT) to set g range
+ *
+ */
+typedef enum {
+  ADXL345_RANGE_16_G = 0b11, ///< +/- 16g
+  ADXL345_RANGE_8_G = 0b10,  ///< +/- 8g
+  ADXL345_RANGE_4_G = 0b01,  ///< +/- 4g
+  ADXL345_RANGE_2_G = 0b00   ///< +/- 2g (default value)
 } range_t;
 
+/**
+ * @brief Class to interact with the ADXL345 accelerometer
+ *
+ */
 class Adafruit_ADXL345_Unified : public Adafruit_Sensor {
- public:
+public:
   Adafruit_ADXL345_Unified(int32_t sensorID = -1);
-  Adafruit_ADXL345_Unified(uint8_t clock, uint8_t miso, uint8_t mosi, uint8_t cs, int32_t sensorID = -1);
+  Adafruit_ADXL345_Unified(uint8_t clock, uint8_t miso, uint8_t mosi,
+                           uint8_t cs, int32_t sensorID = -1);
 
-  bool       begin(uint8_t addr = ADXL345_DEFAULT_ADDRESS);
-  void       setRange(range_t range);
-  range_t    getRange(void);
-  void       setDataRate(dataRate_t dataRate);
+  bool begin(uint8_t addr = ADXL345_DEFAULT_ADDRESS);
+  void setRange(range_t range);
+  range_t getRange(void);
+  void setDataRate(dataRate_t dataRate);
   dataRate_t getDataRate(void);
-  bool       getEvent(sensors_event_t*);
-  void       getSensor(sensor_t*);
+  bool getEvent(sensors_event_t *);
+  void getSensor(sensor_t *);
 
-  uint8_t    getDeviceID(void);
-  void       writeRegister(uint8_t reg, uint8_t value);
-  uint8_t    readRegister(uint8_t reg);
-  int16_t    read16(uint8_t reg);
+  uint8_t getDeviceID(void);
+  void writeRegister(uint8_t reg, uint8_t value);
+  uint8_t readRegister(uint8_t reg);
+  int16_t read16(uint8_t reg);
 
-  int16_t    getX(void), getY(void), getZ(void);
- private:
+  int16_t getX(void), getY(void), getZ(void);
 
-  inline uint8_t  i2cread(void);
-  inline void     i2cwrite(uint8_t x);
+private:
+  inline uint8_t i2cread(void);
+  inline void i2cwrite(uint8_t x);
 
   int32_t _sensorID;
   range_t _range;
   uint8_t _clk, _do, _di, _cs;
-  bool    _i2c;
-  int8_t  _i2caddr;
+  bool _i2c;
+  int8_t _i2caddr;
 };
 
-#endif  // Adafruit_ADXL345_h
+#endif // Adafruit_ADXL345_h
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
+# Adafruit ADXL345 [![Build Status](https://github.com/adafruit/Adafruit_ADXL345/workflows/Arduino%20Library%20CI/badge.svg)](https://github.com/adafruit/Adafruit_ADXL345/actions)![Documentation](https://github.com/adafruit/ci-arduino/blob/master/assets/doxygen_badge.svg)](http://adafruit.github.io/Adafruit_ADXL345/html/index.html)
 # Adafruit ADXL345 Accelerometer Driver
 
 This driver is for the Adafruit ADXL345 Breakout (http://www.adafruit.com/products/1231), and is based on Adafruit's Unified Sensor Library (Adafruit_Sensor).
+Tested and works great with the Adafruit ADXL345 Breakout Board
+[<img src="https://cdn-shop.adafruit.com/970x728/1231-00.jpg" width="500px">](https://www.adafruit.com/products/1231)
+Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
 
 ## About the ADXL345 ##
 
@@ -8,44 +12,24 @@ The ADXL345 is a digital accelerometer that supports both SPI and I2C mode, with
 
 More information on the ADXL345 can be found in the datasheet: http://www.analog.com/static/imported-files/data_sheets/ADXL345.pdf
 
-## What is the Adafruit Unified Sensor Library? ##
-
-The Adafruit Unified Sensor Library (https://github.com/adafruit/Adafruit_Sensor) provides a common interface and data type for any supported sensor.  It defines some basic information about the sensor (sensor limits, etc.), and returns standard SI units of a specific type and scale for each supported sensor type.
-
-It provides a simple abstraction layer between your application and the actual sensor HW, allowing you to drop in any comparable sensor with only one or two lines of code to change in your project (essentially the constructor since the functions to read sensor data and get information about the sensor are defined in the base Adafruit_Sensor class).
-
-This is imporant useful for two reasons:
-
-1.) You can use the data right away because it's already converted to SI units that you understand and can compare, rather than meaningless values like 0..1023.
-
-2.) Because SI units are standardised in the sensor library, you can also do quick sanity checks working with new sensors, or drop in any comparable sensor if you need better sensitivity or if a lower cost unit becomes available, etc. 
-
-Light sensors will always report units in lux, gyroscopes will always report units in rad/s, etc. ... freeing you up to focus on the data, rather than digging through the datasheet to understand what the sensor's raw numbers really mean.
-
-## About this Driver ##
-
-Adafruit invests time and resources providing this open source code.  Please support Adafruit and open-source hardware by purchasing products from Adafruit!
-
-Written by Kevin (KTOWN) Townsend for Adafruit Industries.
-
 <!-- START COMPATIBILITY TABLE -->
 
 ## Compatibility
 
 MCU                | Tested Works | Doesn't Work | Not Tested  | Notes
 ------------------ | :----------: | :----------: | :---------: | -----
-Atmega328 @ 16MHz  |      X       |             |            | 
-Atmega328 @ 12MHz  |      X       |             |            | 
-Atmega32u4 @ 16MHz |      X       |             |            | 
-Atmega32u4 @ 8MHz  |      X       |             |            | 
-ESP8266            |      X       |             |            | 
-Atmega2560 @ 16MHz |      X       |             |            | 
-ATSAM3X8E          |      X       |             |            | 
-ATSAM21D           |      X       |             |            | 
+Atmega328 @ 16MHz  |      X       |             |            |
+Atmega328 @ 12MHz  |      X       |             |            |
+Atmega32u4 @ 16MHz |      X       |             |            |
+Atmega32u4 @ 8MHz  |      X       |             |            |
+ESP8266            |      X       |             |            |
+Atmega2560 @ 16MHz |      X       |             |            |
+ATSAM3X8E          |      X       |             |            |
+ATSAM21D           |      X       |             |            |
 ATtiny85 @ 16MHz   |             |      X       |            | sketch too big
 ATtiny85 @ 8MHz    |             |      X       |            | sketch too big
-Intel Curie @ 32MHz |      X       |             |            | 
-STM32F2            |             |             |     X       | 
+Intel Curie @ 32MHz |      X       |             |            |
+STM32F2            |             |             |     X       |
 
   * ATmega328 @ 16MHz : Arduino UNO, Adafruit Pro Trinket 5V, Adafruit Metro 328, Adafruit Metro Mini
   * ATmega328 @ 12MHz : Adafruit Pro Trinket 3V
@@ -59,3 +43,41 @@ STM32F2            |             |             |     X       |
   * ATtiny85 @ 8MHz : Adafruit Gemma, Arduino Gemma, Adafruit Trinket 3V
 
 <!-- END COMPATIBILITY TABLE -->
+## What is the Adafruit Unified Sensor Library? ##
+
+The Adafruit Unified Sensor Library (https://github.com/adafruit/Adafruit_Sensor) provides a common interface and data type for any supported sensor.  It defines some basic information about the sensor (sensor limits, etc.), and returns standard SI units of a specific type and scale for each supported sensor type.
+
+It provides a simple abstraction layer between your application and the actual sensor HW, allowing you to drop in any comparable sensor with only one or two lines of code to change in your project (essentially the constructor since the functions to read sensor data and get information about the sensor are defined in the base Adafruit_Sensor class).
+
+This is imporant useful for two reasons:
+
+1.) You can use the data right away because it's already converted to SI units that you understand and can compare, rather than meaningless values like 0..1023.
+
+2.) Because SI units are standardised in the sensor library, you can also do quick sanity checks working with new sensors, or drop in any comparable sensor if you need better sensitivity or if a lower cost unit becomes available, etc.
+
+Light sensors will always report units in lux, gyroscopes will always report units in rad/s, etc. ... freeing you up to focus on the data, rather than digging through the datasheet to understand what the sensor's raw numbers really mean.
+
+## About this Driver ##
+
+Adafruit invests time and resources providing this open source code.  Please support Adafruit and open-source hardware by purchasing products from Adafruit!
+# Dependencies
+ * [Adafruit Unified Sensor Driver](https://github.com/adafruit/Adafruit_Sensor)
+
+# Contributing
+Contributions are welcome! Please read our [Code of Conduct](https://github.com/adafruit/Adafruit_ADXL345/blob/master/CODE_OF_CONDUCT.md>)
+before contributing to help this project stay welcoming.
+
+## Documentation and doxygen
+Documentation is produced by doxygen. Contributions should include documentation for any new code added.
+
+Some examples of how to use doxygen can be found in these guide pages:
+
+https://learn.adafruit.com/the-well-automated-arduino-library/doxygen
+
+https://learn.adafruit.com/the-well-automated-arduino-library/doxygen-tips
+
+Written by Kevin (KTOWN) Townsend for Adafruit Industries.
+BSD license, check license.txt for more information
+All text above must be included in any redistribution
+
+To install, use the Arduino Library Manager and search for "Adafruit ADXL345" and install the library.
